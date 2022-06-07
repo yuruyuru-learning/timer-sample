b6193dc144cec84cb99397905d0bc5291a5b5f3b
yukiny0811 <yukiny0811@gmail.com>
2022-06-07 12:50:07 +0900

    sample

--- timerSample/ViewController.swift
+++ timerSample/ViewController.swift
@@ -8,12 +8,32 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    var timer: Timer!
+    var startTime: TimeInterval!
+    var stockTime: TimeInterval!
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        startTime = Date.timeIntervalSinceReferenceDate
+        stockTime = Date.timeIntervalSinceReferenceDate - startTime
     }
-
+    
+    @objc func onTimer() {
+        print(stockTime + Date.timeIntervalSinceReferenceDate - startTime)
+    }
+    
+    @IBAction func start() {
+        startTime = Date.timeIntervalSinceReferenceDate
+        timer = Timer.scheduledTimer(timeInterval: 0.1, target: self, selector: #selector(onTimer), userInfo: nil, repeats: true)
+    }
+    
+    @IBAction func stop() {
+        timer.invalidate()
+        stockTime += Date.timeIntervalSinceReferenceDate - startTime
+    }
+    
 
 }
 
